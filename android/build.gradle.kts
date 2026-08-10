344b2ead1f9f706329f8d4191b5b5d41fe83302b
liebertjohan-art <liebertjohan-art@users.noreply.github.com>
2026-08-10 19:49:51 +0000

    fix(android): dynamically set namespace for old plugins (isar) in AGP 8+

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,3 +19,22 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    afterEvaluate {
+        val androidExt = project.extensions.findByName("android")
+        if (androidExt != null) {
+            try {
+                val clazz = androidExt.javaClass
+                val getNamespace = clazz.getMethod("getNamespace")
+                val namespace = getNamespace.invoke(androidExt)
+                if (namespace == null) {
+                    val setNamespace = clazz.getMethod("setNamespace", String::class.java)
+                    setNamespace.invoke(androidExt, project.group.toString())
+                }
+            } catch (e: Exception) {
+                // Ignore
+            }
+        }
+    }
+}
